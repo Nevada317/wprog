4a244f28bf28cff69d9f222e21a6ab6b2c1c6b12
Vladimir <nevada@live.ru>
2020-12-26 21:41:16 +0300

    Loop added

--- src/main.asm
+++ src/main.asm
@@ -1,6 +1,114 @@
 .include "m324_custom.inc"
 
 .cseg
-.org 0
+.org addr_RESET
+	jmp RESET
 
-jmp 0
+; ##############################
+; ####   INTERRUPTS TABLE   ####
+; ##############################
+
+.org addr_INT0
+	reti
+	nop
+.org addr_INT1
+	reti
+	nop
+.org addr_INT2
+	reti
+	nop
+.org addr_PCINT0
+	reti
+	nop
+.org addr_PCINT1
+	reti
+	nop
+.org addr_PCINT2
+	reti
+	nop
+.org addr_PCINT3
+	reti
+	nop
+.org addr_WDT
+	reti
+	nop
+.org addr_TIMER2_COMPA
+	reti
+	nop
+.org addr_TIMER2_COMPB
+	reti
+	nop
+.org addr_TIMER2_OVF
+	reti
+	nop
+.org addr_TIMER1_CAPT
+	reti
+	nop
+.org addr_TIMER1_COMPA
+	reti
+	nop
+.org addr_TIMER1_COMPB
+	reti
+	nop
+.org addr_TIMER1_OVF
+	reti
+	nop
+.org addr_TIMER0_COMPA
+	reti
+	nop
+.org addr_TIMER0_COMPB
+	reti
+	nop
+.org addr_TIMER0_OVF
+	reti
+	nop
+.org addr_SPI_STC
+	reti
+	nop
+.org addr_USART0_RX
+	reti
+	nop
+.org addr_USART0_UDRE
+	reti
+	nop
+.org addr_USART0_TX
+	reti
+	nop
+.org addr_ANALOG_COMP
+	reti
+	nop
+.org addr_ADC
+	reti
+	nop
+.org addr_EE_READY
+	reti
+	nop
+.org addr_TWI
+	reti
+	nop
+.org addr_SPM_READY
+	reti
+	nop
+.org addr_USART1_RX
+	reti
+	nop
+.org addr_USART1_UDRE
+	reti
+	nop
+.org addr_USART1_TX
+	reti
+	nop
+
+.org addr_ISR_END
+; ####################################
+; ####   INTERRUPTS TABLE ENDED   ####
+; ####################################
+
+RESET:
+	.include "init.asm"
+
+Loop:
+	.include "loop.asm"
+rjmp Loop
+
+
