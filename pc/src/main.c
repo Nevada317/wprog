d560bda6c4fdaf594b1fed3a531f7e72dfa8290f
Vladimir (np305) <nevada@live.ru>
2021-01-05 21:47:45 +0300

    Add variable to store input string (port)

--- pc/src/main.c
+++ pc/src/main.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 	printf("Program started\n");
+
+	char* strPort = NULL;
+
+	printf("Parsing input parameters...\n");
+	// https://www.geeksforgeeks.org/getopt-function-in-c-to-parse-command-line-arguments/
 	int opt;
 	while((opt = getopt(argc, argv, ":p:")) != -1) {
 		switch (opt) {
 			case 'p':
-				printf("Port is %s\n", optarg);
+				if (strPort) {
+					free(strPort);
+					strPort = NULL;
+				}
+				strPort = malloc(strlen(optarg)+1);
+				strcpy(strPort, optarg);
+				printf("  Port is \"%s\"\n", strPort);
 				break;
 			case ':':
-				printf("option %c needs a value\n", opt);
+				printf("  Option %c needs a value\n", opt);
 				break;
 			case '?':
-				printf("unknown option: %c\n", optopt);
+				printf("  Unknown option: %c\n", optopt);
 				break;
 		}
 	}
+	printf("Parsing input parameters - done!\n\n");
 
 	printf("Program finishes\n");
+
+	if (strPort) {
+		free(strPort);
+		strPort = NULL;
+	}
+
 	return 0;
 }
