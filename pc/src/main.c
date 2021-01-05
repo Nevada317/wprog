7e7583006d3fcc9ce6891d65dbf957cb55f8715f
Vladimir (np305) <nevada@live.ru>
2021-01-05 21:28:51 +0300

    Option parser added

--- pc/src/main.c
+++ pc/src/main.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+	printf("Program started\n");
+	int opt;
+	while((opt = getopt(argc, argv, ":p:")) != -1) {
+		switch (opt) {
+			case 'p':
+				printf("Port is %s\n", optarg);
+				break;
+			case ':':
+				printf("option %c needs a value\n", opt);
+				break;
+			case '?':
+				printf("unknown option: %c\n", optopt);
+				break;
+		}
+	}
 
+	printf("Program finishes\n");
 	return 0;
 }
